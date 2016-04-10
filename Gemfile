3661bae345ea538cc6cb67d71538237634e0c1b2
JensOmfjord <jens.omfjord@gmail.com>
2016-04-09 20:55:24 -0700

    Database set up and being written to from the website

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.6'
 gem 'devise'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+gem 'mysql2'
 
 gem 'bootstrap-sass', '~> 3.3.6'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -13,28 +13,62 @@
 
 ActiveRecord::Schema.define(version: 20160409205230) do
 
+  create_table "cards", force: :cascade do |t|
+    t.integer "value", limit: 4, null: false
+    t.string  "color", limit: 6
+  end
+
+  create_table "chats", id: false, force: :cascade do |t|
+    t.integer "message_id", limit: 4, null: false
+    t.integer "game_id",    limit: 4, null: false
+  end
+
+  add_index "chats", ["game_id"], name: "game_id", using: :btree
+
+  create_table "game_tables", id: false, force: :cascade do |t|
+    t.integer "game_id",      limit: 4, null: false
+    t.integer "card_id",      limit: 4, null: false
+    t.integer "user_id",      limit: 4
+    t.integer "card_in_deck", limit: 4, null: false
+    t.integer "card_played",  limit: 4, null: false
+  end
+
+  add_index "game_tables", ["card_id"], name: "card_id", using: :btree
+
+  create_table "game_users", id: false, force: :cascade do |t|
+    t.integer "user_id", limit: 4, null: false
+    t.integer "game_id", limit: 4, null: false
+  end
+
+  add_index "game_users", ["game_id"], name: "game_id", using: :btree
+
   create_table "games", force: :cascade do |t|
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "host_id", limit: 4, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "user_id", limit: 4,   null: false
+    t.string   "message", limit: 512
+    t.datetime "time"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "avatar",                 limit: 60
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
