61d28e84b31c63291392a405e91e3f495f86e80e
JensOmfjord <jens.omfjord@gmail.com>
2016-04-15 11:45:14 -0700

    Added avatar to users

--- db/schema.rb
+++ db/schema.rb
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160413172430) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
