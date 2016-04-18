177aa7e0fa1ad0b23da522249f62da134636f496
FredrikSigvartsen <fredrik.sigvartsen@gmail.com>
2016-04-18 16:47:24 -0700

    Typo

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ActiveRecord::Base
   # Attributes: :id, :user_id, :message, :time, :created_at, :updated_at
 
-  validates :user_id, presesence: true
+  validates :user_id, presence: true
   validates :message, presence: true
   
   belongs_to :chat
