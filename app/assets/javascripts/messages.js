070cd12326d354ea3cb24204c7d528059e1b20c2
Izaac Garcilazo <izaacgarcilazo@msn.com>
2016-05-05 12:05:57 -0700

    added chat scroll bar, fix empty message

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('.messages').animate({
+        scrollTop: $('.messages')[0].scrollHeight}, 2000);
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,19 +7,14 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(params[:message])
-    t = Time.now()
-    email = current_user[:email]
-    emailusername = email[/[^@]+/]
-
-
-    Pusher.trigger('chat', 'new_message', {
-      timestamp:t.strftime("%H:%M:%S"),
-      name: emailusername,
-      message: @message.message, 
-    }, {
-      socket_id: params[:socket_id]
-    })
-
-    respond_to :js
+    if(@message.message != '')
+      t = Time.now()
+      email = current_user[:email]
+      emailusername = email[/[^@]+/]
+      Pusher.trigger('chat', 'new_message', { timestamp:t.strftime("%H:%M:%S"),
+        name: emailusername, message: @message.message, 
+      }, { socket_id: params[:socket_id] })
+      respond_to :js
+    end
   end
 end
