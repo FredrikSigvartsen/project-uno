2acbe53e90801be22b91bf3f312cd4609ba87309
Izaac Garcilazo <izaacgarcilazo@msn.com>
2016-05-16 15:41:08 -0700

    add chat

--- app/controllers/game_tables_controller.rb
+++ app/controllers/game_tables_controller.rb
@@ -1,3 +1,3 @@
 class GameTablesController < ApplicationController
-	
+ 	
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,8 @@
 class GamesController < ApplicationController
 
   def index
-    render @game 
     @message = Message.new
+    render @game 
   end
 
   def newgame
