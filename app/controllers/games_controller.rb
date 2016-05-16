4f942c025e0973864ed663047807bf3a668f7b62
FredrikSigvartsen <fredrik.sigvartsen@gmail.com>
2016-05-15 18:17:07 -0700

    Added more fields to send through channel Lobby when a game is created

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,8 +18,9 @@ class GamesController < ApplicationController
 
     if @game.save
       redirect_to games_index_path
+      user = User.find(@game.host_id)
       Pusher.trigger("lobby", "new_game",
-                     {game_id: @game.id})
+        {game_id: @game.id}, description: @game.description, host: user.username, number_of_players: @game.users.length)
     else
       render 'new'
     end
@@ -35,7 +36,8 @@ class GamesController < ApplicationController
   end
 
   private
-  def game_params
-    params.require(:game).permit(:description)
-  end
+    def game_params
+      params.require(:game).permit(:description)
+    end
 end
+#Add logic for playing card
